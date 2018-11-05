ffdf1b5a8134e30c61ee32fed0ba8e408c86f221
Jonathan Soll <soll@rembrandtip.com>
2018-11-04 20:54:04 -0500

    committing

--- IntradayPrice2.py
+++ IntradayPrice2.py
@@ -74,15 +74,20 @@ def getEndDate(eventDate, num_days):
         if endDate.weekday() not in [5, 6]:
             counter += 1
         if counter == num_days:
-            return endDate
+            return endDate.date()
 
-def _utc_to_est(datetimes):
-    return datetimes + datetime.timedelta(hours = -4)
+def _utc_to_est(dt):
+    return dt + datetime.timedelta(hours = -4)
 
-def _to_dataframe(data):
+def _est_to_utc(dt):
+    return dt + datetime.timedelta(hours = 4)
+
+def _to_dataframe(data, ticker, eventDatetime):
     "Convert list of dicts of time-series price data to pandas dataframe"
     result = pd.DataFrame.from_dict(data)
-    result['time'] = _utc_to_est(result['time'])
+    result['time']          = [_utc_to_est(x) for x in result['time']]
+    result['ticker']        = ticker
+    result['eventDatetime'] = eventDatetime
     return result.set_index('time')
 
 
@@ -121,91 +126,47 @@ def request_price_data(tickers, eventDatetimes):
     request = refDataService.createRequest("IntradayBarRequest")
 
     request.set("eventType", "TRADE") # last price
-    request.set("interval", 15)  # bar interval in minutes
+    request.set("interval", 1)  # bar interval in minutes
 
+    final_result = []
     for ticker, eventDatetime in zip(tickers, eventDatetimes):
+        print('Pulling data for event that happened at %s with %s' % (eventDatetime, ticker))
         request.set("startDateTime", eventDatetime)
         request.set("endDateTime"  , getEndDate(eventDatetime, 10))
         request.set("security", ticker + " Equity")
-        print(request)
+        session.sendRequest(request)
 
+        try:
+            # Process received events
+            data = []
+            while(True):
+                # We provide timeout to give the chance to Ctrl+C handling:
+                ev = session.nextEvent(500)
 
+                flds = ['time', 'close', 'volume']
 
-def main(ticker, eventDateTime, daysPre=5, daysPost=5, interval=15):
-    global options
+                for msg in ev:
+                    if msg.messageType() == 'IntradayBarResponse':
+                        barTickData = msg.getElement('barData').getElement('barTickData')
+                        for i in range(barTickData.numValues()):
+                            data_bar = {}
+                            for fld in flds:
+                                dt = barTickData.getValue(i).getElement(0).getValue()       # dt is in utc
+                                val = (barTickData.getValue(i).getElement(fld).getValue())
+                                data_bar[fld] = val
+                            # print('dt: %s -- eventdt: %s' % (dt, _est_to_utc(eventDatetime)))
+                            if dt >= _est_to_utc(eventDatetime):                            # to compare utc to utc
+                                data.append(data_bar)
 
-    sessionOptions = blpapi.SessionOptions()
+                # Response completly received, so we could exit
+                if ev.eventType() == blpapi.Event.RESPONSE:
+                    break
+            final_result.append(_to_dataframe(data, ticker, eventDatetime))
 
-    try:
-        options = parseCmdLine()
-        # Fill SessionOptions
-        host = options.host
-        port = options.port
-    except:
-        host = 'localhost'
-        port = 8194
+        except Exception as e:
+            print('Error:', e)
 
-    sessionOptions.setServerHost(host)
-    sessionOptions.setServerPort(port)
-
-    print("\n\nConnecting to %s:%d\n" % (host, port))
-
-    # Create a Session
-    session = blpapi.Session(sessionOptions)
-
-    # Start a Session
-    if not session.start():
-        print("Failed to start session.")
-        return
-
-    if not session.openService("//blp/refdata"):
-        print("Failed to open //blp/refdata")
-        return
-
-    refDataService = session.getService("//blp/refdata")
-    request = refDataService.createRequest("IntradayBarRequest")
-    request.set("security", ticker + " US Equity")
-    request.set("eventType", "TRADE") # last price
-    request.set("interval", interval)  # bar interval in minutes
-
-
-    request.set("startDateTime", getStartDate(eventDateTime, daysPre))
-    request.set("endDateTime"  , getEndDate(eventDateTime, daysPost))
-
-
-
-    # print("Sending Request:", request)
-    session.sendRequest(request)
-
-    print('hello')
-    try:
-
-        # Process received events
-        data = []
-        while(True):
-            # We provide timeout to give the chance to Ctrl+C handling:
-            ev = session.nextEvent(500)
-
-            flds = ['time', 'close', 'volume']
-
-            for msg in ev:
-                if msg.messageType() == 'IntradayBarResponse':
-                    barTickData = msg.getElement('barData').getElement('barTickData')
-                    for i in range(barTickData.numValues()):
-                        data_bar = {}
-                        for fld in flds:
-                            dt = barTickData.getValue(i).getElement(0).getValue()
-                            val = (barTickData.getValue(i).getElement(fld).getValue())
-                            data_bar[fld] = val
-                        data.append(data_bar)
-
-            # Response completly received, so we could exit
-            if ev.eventType() == blpapi.Event.RESPONSE:
-                break
-
-        return _to_dataframe(data)
-
-    finally:
-        # Stop the session
-        session.stop()
-        print('Session Closed')
+    # Stop the session
+    session.stop()
+    print('Session Closed')
+    return final_result
